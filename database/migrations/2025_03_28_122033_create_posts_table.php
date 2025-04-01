a49d98f4e90e04df07b02312685be28becd8033c
ShubhamDev021 <shubhat021@gmail.com>
2025-04-01 13:03:35 +0530

    All tables creation

--- database/migrations/2025_03_28_122033_create_posts_table.php
+++ database/migrations/2025_03_28_122033_create_posts_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->foreignId('user_id')->contrained();
+            $table->foreignId('category_id')->contrained();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
